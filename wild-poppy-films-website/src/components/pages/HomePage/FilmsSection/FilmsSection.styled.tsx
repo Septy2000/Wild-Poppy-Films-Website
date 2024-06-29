66ff51414648ed219ec5155c134525aec716726a
Septy28 <calin.septimiu28@gmail.com>
2024-06-29 21:58:25 +0100

    Applied background to home page films section based on screen size;

--- wild-poppy-films-website/src/components/pages/HomePage/FilmsSection/FilmsSection.styled.tsx
+++ wild-poppy-films-website/src/components/pages/HomePage/FilmsSection/FilmsSection.styled.tsx
@@ -1,12 +1,22 @@
 "use client";
 import styled from "styled-components";
+import HomePageFilmsBackgroundMobile from "@/images/home-page/films/home-films-background-mobile.png";
+import HomePageFilmsBackgroundTablet from "@/images/home-page/films/home-films-background-tablet.png";
+import HomePageFilmsBackgroundDesktop from "@/images/home-page/films/home-films-background-desktop.png";
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-image: url(${HomePageFilmsBackgroundMobile.src});
+    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
+        background-image: url(${HomePageFilmsBackgroundTablet.src});
+    }
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        background-image: url(${HomePageFilmsBackgroundDesktop.src});
+    }
 `;
 
 export const FilmsContainer = styled.div`
-    padding: 20px 48px;
+    padding: 48px 20px 16px 20px;
 `;
