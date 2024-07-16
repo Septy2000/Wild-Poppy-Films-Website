3c1d562a34a443dbf26e6a1cdea26d02e532822a
Septy28 <calin.septimiu28@gmail.com>
2024-07-16 15:22:46 +0100

    Finished website footer;

--- wild-poppy-films-website/src/components/Footer/Footer.styled.tsx
+++ wild-poppy-films-website/src/components/Footer/Footer.styled.tsx
@@ -1,5 +1,11 @@
 "use client";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import WPLogoAltXl from "@/icons/logo/wp-logo-alt-xl.svg";
+import FacebookIcon from "@/icons/social/fb-icon.svg";
+import InstagramIcon from "@/icons/social/ig-icon.svg";
+import LinkedInIcon from "@/icons/social/linkedin-icon.svg";
+import YouTubeIcon from "@/icons/social/yt-icon.svg";
+import XIcon from "@/icons/social/x-icon.svg";
 
 export const Container = styled.footer`
     display: flex;
@@ -17,7 +23,81 @@ export const Separator = styled.div`
     background: ${({ theme }) => theme.colors.secondary_shaded.celadon_blue};
 `;
 
-export const TestDiv = styled.div`
-    height: 100px;
-    width: 100%;
+export const LogoSocialsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    align-items: start;
+`;
+
+export const WildPoppyLogoAltXl = styled(WPLogoAltXl)`
+    width: 96px;
+    height: 64px;
+`;
+
+export const SocialsContainer = styled.div`
+    display: flex;
+    gap: 12px;
+
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const PagesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    align-items: flex-start;
+    justify-content: flex-start;
+`;
+
+const IconStylingCss = css`
+    path {
+        fill: ${({ theme }) => theme.colors.primary.poppy_red};
+    }
+`;
+
+export const FacebookIconStyled = styled(FacebookIcon)`
+    ${IconStylingCss}
+`;
+
+export const InstagramIconStyled = styled(InstagramIcon)`
+    ${IconStylingCss}
+`;
+
+export const LinkedInIconStyled = styled(LinkedInIcon)`
+    ${IconStylingCss}
+`;
+
+export const YouTubeIconStyled = styled(YouTubeIcon)`
+    path:nth-child(1) {
+        fill: ${({ theme }) => theme.colors.primary.poppy_red};
+    }
+`;
+
+export const XIconStyled = styled(XIcon)`
+    ${IconStylingCss}
+`;
+
+export const LegalContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+`;
+
+export const ColFlexContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
+
+export const CopyrightText = styled.span`
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%; /* 15px */
+    letter-spacing: -0.22px;
+    color: ${({ theme }) => theme.colors.neutral.neutral_4};
 `;
--- wild-poppy-films-website/src/components/Footer/Footer.tsx
+++ wild-poppy-films-website/src/components/Footer/Footer.tsx
@@ -1,13 +1,54 @@
 import * as Styled from "./Footer.styled";
+import ModalLinkButton from "@/components/Buttons/ModalLinkButton/ModalLinkButton";
+import ModalSocialButton from "@/components/Buttons/ModalSocialButton/ModalSocialButton";
+import { LegalButton } from "@/components/Buttons/LegalButton/LegalButton.styled";
 
 export default function Footer() {
+    const pagesItems: { label: string; link: string }[] = [
+        { label: "FILMS", link: "/films" },
+        { label: "OUR TEAM", link: "/our-team" },
+        { label: "CONTACT", link: "/contact" },
+        { label: "SUPPORT US", link: "/support-us" },
+    ];
+
+    const socialItems: { icon: React.JSX.Element; link: string }[] = [
+        { icon: <Styled.InstagramIconStyled />, link: "https://www.instagram.com" },
+        { icon: <Styled.XIconStyled />, link: "https://www.x.com" },
+        { icon: <Styled.FacebookIconStyled />, link: "https://www.facebook.com" },
+        { icon: <Styled.YouTubeIconStyled />, link: "https://www.youtube.com" },
+        { icon: <Styled.LinkedInIconStyled />, link: "https://www.linkedin.com" },
+    ];
     return (
         <Styled.Container>
-            <Styled.TestDiv>Test</Styled.TestDiv>
+            <Styled.LogoSocialsContainer>
+                <Styled.WildPoppyLogoAltXl />
+                <Styled.SocialsContainer>
+                    {socialItems.map((item, id) => (
+                        <ModalSocialButton key={id} icon={item.icon} link={item.link} />
+                    ))}
+                </Styled.SocialsContainer>
+            </Styled.LogoSocialsContainer>
             <Styled.Separator />
-            <Styled.TestDiv>Test</Styled.TestDiv>
+            <Styled.PagesContainer>
+                {pagesItems.map((page, id) => (
+                    <ModalLinkButton
+                        key={id}
+                        label={page.label}
+                        link={page.link}
+                        translateX={0}
+                        isVisible={true}
+                        delay={0}
+                    />
+                ))}
+            </Styled.PagesContainer>
             <Styled.Separator />
-            <Styled.TestDiv>Test</Styled.TestDiv>
+            <Styled.LegalContainer>
+                <Styled.ColFlexContainer>
+                    <LegalButton href={"/terms-and-conditions"}>{`TERMS & CONDITIONS`}</LegalButton>
+                    <LegalButton href={"/cookie-policy"}>{`COOKIE POLICY`}</LegalButton>
+                </Styled.ColFlexContainer>
+                <Styled.CopyrightText>{"©️ WILD POPPY FILMS, 2024"}</Styled.CopyrightText>
+            </Styled.LegalContainer>
         </Styled.Container>
     );
 }
