02f70c71b3f32a12cfff8b2c07fbc8f970d46b8b
Septy28 <calin.septimiu28@gmail.com>
2024-07-21 12:24:04 +0100

    Added films filter;

--- wild-poppy-films-website/src/components/pages/FilmsPage/FilmsPage.styled.tsx
+++ wild-poppy-films-website/src/components/pages/FilmsPage/FilmsPage.styled.tsx
@@ -3,10 +3,43 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     margin: 0 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const FilmsContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 24px;
+`;
+
+export const FilmsFilterContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-shrink: 0;
+    max-width: 320px;
+    width: 100%;
+    margin-bottom: 24px;
+`;
+
+export const FilmsFilter = styled.button<{ $selected: boolean }>`
+    border: ${({ theme }) => theme.colors.neutral.neutral_12} 1px solid;
+    padding: 16px 0;
+    width: 100%;
+    &:first-child {
+        border-radius: 16px 0 0 16px;
+    }
+
+    &:last-child {
+        border-radius: 0 16px 16px 0;
+    }
+
+    &:hover {
+        opacity: ${({ $selected }) => ($selected ? "none" : "60%")};
+    }
+
+    background: ${({ theme, $selected }) =>
+        $selected ? theme.colors.secondary.core_green_light_2 : theme.colors.neutral.neutral_1};
 `;
--- wild-poppy-films-website/src/components/pages/FilmsPage/FilmsPage.tsx
+++ wild-poppy-films-website/src/components/pages/FilmsPage/FilmsPage.tsx
@@ -1,9 +1,46 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import FilmContainerLarge from "@/components/pages/FilmsPage/FilmContainerLarge/FilmContainerLarge";
 import * as Styled from "./FilmsPage.styled";
 import { films } from "@/data";
+
+type FilterOptions = "all" | "available" | "coming_soon";
+
 export function FilmsPage() {
+    const [selectedFilmFilter, setSelectedFilmFilter] = useState<FilterOptions>("all");
+    const filters: { label: string; status: FilterOptions }[] = [
+        {
+            label: "ALL",
+            status: "all",
+        },
+        {
+            label: "AVAILABLE",
+            status: "available",
+        },
+        {
+            label: "COMING SOON",
+            status: "coming_soon",
+        },
+    ];
+
+    useEffect(() => {
+        console.log(selectedFilmFilter);
+    }, [selectedFilmFilter]);
+
     return (
         <Styled.Container>
+            <div style={{ height: "100px" }}></div>
+            <Styled.FilmsFilterContainer>
+                {filters.map((filter, index) => (
+                    <Styled.FilmsFilter
+                        key={index}
+                        onClick={() => setSelectedFilmFilter(filter.status)}
+                        $selected={selectedFilmFilter === filter.status}
+                    >
+                        {filter.label}
+                    </Styled.FilmsFilter>
+                ))}
+            </Styled.FilmsFilterContainer>
             <Styled.FilmsContainer>
                 {films.map((film, index) => (
                     <FilmContainerLarge key={index} film={film} />
