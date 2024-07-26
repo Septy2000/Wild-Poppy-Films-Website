325e279dc4ae3fb4494925321478a7f0a6b0ec4e
Septy28 <calin.septimiu28@gmail.com>
2024-07-26 21:50:34 +0100

    Implemented carousel section on home page for desktop

--- wild-poppy-films-website/src/components/pages/HomePage/CarouselSection/CarouselSection.styled.tsx
+++ wild-poppy-films-website/src/components/pages/HomePage/CarouselSection/CarouselSection.styled.tsx
@@ -4,15 +4,24 @@ import styled, { keyframes, css } from "styled-components";
 import PoppyLogoSketch from "@/icons/logo/poppy-logo-sketch.svg";
 
 export const Container = styled.div`
-    overflow: hidden;
-    padding: 48px 20px 120px 20px;
+    position: relative;
+    padding: 3rem 1.25rem 7.5rem 1.25rem;
+
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        padding: 7.5rem 8rem;
+    }
 `;
 
 export const CarouselContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
-    gap: 24px;
+    gap: 1.5rem;
+
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
 export const CarouselLeft = styled.div`
@@ -21,6 +30,14 @@ export const CarouselLeft = styled.div`
     margin-top: 28px;
     justify-items: flex-start;
     align-items: start;
+
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        margin: 0;
+        gap: 2rem;
+        flex-direction: row;
+        justify-items: center;
+        align-items: center;
+    }
 `;
 
 export const CarouselRight = styled(CarouselLeft)`
@@ -48,11 +65,22 @@ export const CarouselItem = styled(Image)<{ $translateX: number; $delay: number 
     margin-bottom: 1rem;
 
     ${({ $translateX, $delay }) => slideAnimation($translateX, $delay)}
+
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        margin: 0;
+        width: 18.5rem;
+        height: 18.5rem;
+    }
 `;
 
 export const PoppyLogo = styled(PoppyLogoSketch)`
-    position: relative;
-    top: -8rem;
+    position: absolute;
+    top: 65%;
     right: -6rem;
     z-index: -1;
+
+    @media (min-width: ${({ theme }) => theme.screen.desktop}) {
+        right: -8rem;
+        scale: 2;
+    }
 `;
